426a2683e8486ea3863c76f486fa887e5601fa9d
Ozgur Firat Cinar <ozgurfiratcinar@gmail.com>
2016-04-04 17:51:57 +0300

    added freespace checker feature

--- paste.py
+++ paste.py
@@ -6,6 +6,7 @@ from w3lib.encoding import html_to_unicode as cutf
 import lxml.html
 import os.path
 from free_disk_space_check import disk_usage
+from sys import argv
 
 # check free diskspace
 def check_disk_usage():
@@ -14,6 +15,14 @@ def check_disk_usage():
     used_space = float(current_disk_usage.used)
     free_space = float(current_disk_usage.free)
     return total_space, used_space, free_space
+
+# check freespace higher than X
+def freespace_higher_than(percent):
+    total, used, free = check_disk_usage()
+    if free / total > percent:
+        return True
+    else:
+        return False
 
 # edit link for the suitable format
 def edit_links(html_source, home_url):
@@ -29,19 +38,33 @@ def dump(url, file_name):
 # main url for 'the archive' page
 url = 'http://pastebin.com/archive'
 
-# infinite loop
-while True:
-    print check_disk_usage()
-    r = requests.get(url).content
-    u = cutf(None, r)[1]
-    h = edit_links(u, url)
-    s = bs(h, "lxml")
-    for i in s.find_all('table', class_=re.compile('(?i)^maintable$')):
-        for j in i.find_all('a', href=re.compile('.*')):
-            link = '%s' % j.get('href')
-            if len(re.findall('(?i)archive', link)) > 0: continue
-            file_name = '%s.txt' % re.sub('.+/', '', link)
-            if os.path.isfile(file_name): continue
-            raw_link = 'http://pastebin.com/raw.php?i=%s' % re.sub('.+/', '', link)
-            dump(raw_link, file_name)
-    time.sleep(1)
+def main(percent_by_user=None):
+    # infinite loop
+    while True and freespace_higher_than(percent_by_user):
+        # total, used, free = check_disk_usage()
+        # print("Total: " + total)
+        # print("Used: " + used)
+        # print("Free: " + free)
+
+        r = requests.get(url).content
+        u = cutf(None, r)[1]
+        h = edit_links(u, url)
+        s = bs(h, "lxml")
+        for i in s.find_all('table', class_=re.compile('(?i)^maintable$')):
+            for j in i.find_all('a', href=re.compile('.*')):
+                link = '%s' % j.get('href')
+                if len(re.findall('(?i)archive', link)) > 0: continue
+                file_name = '%s.txt' % re.sub('.+/', '', link)
+                if os.path.isfile(file_name): continue
+                raw_link = 'http://pastebin.com/raw.php?i=%s' % re.sub('.+/', '', link)
+                dump(raw_link, file_name)
+        time.sleep(1)
+    print("Not enough freespace on disk!")
+
+if __name__ == "__main__":
+    print("Started to dump...")
+    if len(argv) == 2:
+        main(int(argv[1]))
+    else:
+        print("Enter an argument for freespace percentage on disk, sample usage:")
+        print("python paste.py 20")
